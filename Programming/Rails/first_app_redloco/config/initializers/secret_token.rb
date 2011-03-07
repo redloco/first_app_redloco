8cc37ff1f8265142a4dbe807653016a799a139e3
DA Morris <redloco@gmail.com>
2011-03-07 20:02:25 +0900

    Initial commit

---
+++ Programming/Rails/first_app_redloco/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppRedloco::Application.config.secret_token = '8dabdd73fbae14af14056a52365d87163c0ddd77960c00113923946900d4dfad814458dc372616e2cb26df390d24c6b3ecc7390a49d5006b620631ea50bcb084'
